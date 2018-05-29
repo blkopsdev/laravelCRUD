3db84396bd869fd91676ce9b44805f432ed5de60
blkopsdev <blkopsdev@gmail.com>
2018-05-29 10:58:58 -0400

    initial commit

---
+++ resources/views/products/edit.blade.php
@@ -0,0 +1,14 @@
+<!-- edit.blade.php -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel 5.5 CRUD Tutorial With Example From Scratch </title>
+</head>
+<body>
+    
+</body>
+</html>
